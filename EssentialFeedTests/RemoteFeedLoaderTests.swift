19a03d46474b1f5674856653146203791e68b5d7
kshitij <kshitij.gupta@swiggy.in>
2021-06-10 21:01:55 +0530

    Remove the generic error type from LoadFeedResult

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -154,7 +154,7 @@ class RemoteFeedLoaderTests: XCTestCase {
             switch(receivedResult, expectedResult) {
             case let (.success(receivedItems), .success(expectedItems)):
                 XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
-            case let (.failure(receivedError), .failure(expectedError)):
+            case let (.failure(receivedError as RemoteFeedLoader.Error), .failure(expectedError as RemoteFeedLoader.Error )):
                 XCTAssertEqual(receivedError, expectedError, file: file, line: line)
                 
             default:
