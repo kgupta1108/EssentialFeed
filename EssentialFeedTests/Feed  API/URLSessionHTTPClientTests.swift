0a3e7a1de8e395faeef7f4be436c42044971f86b
kshitij <kshitij.gupta@swiggy.in>
2021-06-17 10:26:54 +0530

    Delivers Data and Response on successful HTTP URL Request

--- EssentialFeedTests/Feed  API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed  API/URLSessionHTTPClientTests.swift
@@ -18,9 +18,11 @@ class URLSessionHTTPClient {
     struct UnexpectedValuesRepresentation: Error {}
     
     func get(from url: URL, completion: @escaping ((Result) -> Void)) {
-        session.dataTask(with: url) { (_, _, error) in
+        session.dataTask(with: url) { (data, response, error) in
             if let error = error {
                 completion(.failure(error))
+            } else if let data = data, data.count > 0, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
             } else {
                 completion(.failure(UnexpectedValuesRepresentation()))
             }
@@ -72,6 +74,25 @@ class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertNotNil(resultErrorFor(data: anyData(), response: nonHTTPUrlResponse(), error: nil ))
     }
     
+    func test_getFromURL_succeedsOnHTTPUrlResponseWithData(){
+        let data = anyData()
+        let response = anyHTTPUrlResponse()
+        URLProtocolStub.stub(data: data, response: response, error: nil)
+        let exp = expectation(description: "wait for request")
+        makeSUT().get(from: anyUrl()) { result in
+            switch result {
+            case let .success(receivedData, receivedResponse):
+                XCTAssertEqual(receivedData, data)
+                XCTAssertEqual(receivedResponse.url, response.url)
+                XCTAssertEqual(receivedResponse.statusCode, response.statusCode)
+            default:
+                XCTFail("Expected success got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     //MARK: Helpers
     
     private func anyData() -> Data {
@@ -109,7 +130,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         return receivedError
     }
     
-    private func makeSUT() -> URLSessionHTTPClient {
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> URLSessionHTTPClient {
         let sut = URLSessionHTTPClient()
         checkForMemoryLeaks(sut, file: #file, line: #line)
         return sut
